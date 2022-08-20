c78c10c23b9027818c4c4580c7fb72a1156c18ea
Matteo <matteo@pop-os.localdomain>
2022-08-20 11:23:56 +0200

    auto yes commit

--- linux_install.sh
+++ linux_install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
-sudo apt update && sudo apt -y install nala
+sudo apt update
+sudo apt -y install nala
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
 sudo rm microsoft.gpg
-sudo nala update && sudo nala -y install microsoft-edge-stable
+
+sudo nala update
+sudo nala -y install microsoft-edge-stable
 sudo nala -y upgrade
